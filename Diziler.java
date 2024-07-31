d9b2166edeb2f9b5e03d87d1dc05bfb38aa28073
Ertuğrul Kıraç <40743235+ertugrulkirac@users.noreply.github.com>
2024-07-31 23:48:08 +0300

    Add files via upload

---
+++ Diziler.java
@@ -0,0 +1,19 @@
+public class Diziler {
+
+    public static void main(String[] args) {
+
+        String [] aylar = {"Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz"};
+
+        System.out.printf( aylar[0] + "\n");
+        System.out.printf( aylar[1] + "\n");
+        System.out.printf( aylar[2] + "\n");
+        System.out.printf( aylar[3] + "\n");
+
+        for (int i = 0; i < aylar.length; i++) {
+
+            System.out.printf( aylar[i] + "\n");
+        }
+
+
+    }
+}
